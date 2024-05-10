a863ad650dd75f54e9d072f49f55f6651712c190
arthitsaha <66513838+arthitsaha@users.noreply.github.com>
2024-05-10 21:47:39 +0530

    Delete attendencesystem directory

--- attendencesystem/admin.py
+++
@@ -1,6 +0,0 @@
-# attendancesystem/admin.py
-from django.contrib import admin
-from .models import Employee, Attendance
-
-admin.site.register(Employee)
-admin.site.register(Attendance)
--- attendencesystem/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AttendencesystemConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "attendencesystem"
